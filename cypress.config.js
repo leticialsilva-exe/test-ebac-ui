50a3f6854a3d840badcaf7cb2a98f83a8ee9b970
leticialsilva-exe <leticial.silva@outlook.com>
2024-12-09 23:13:30 -0300

    alterando git ignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'kx4id3',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
 
-    baseUrl: 'http://lojaebac.ebaconline.art.br'
+    baseUrl: 'http://lojaebac.ebaconline.art.br',
+    video: true
   },
 });
